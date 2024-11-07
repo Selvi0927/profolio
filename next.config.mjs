d6ef6c48835805b53438ff78a0cd7bf0ea8b6479
Selvi0927 <balaselvi0927@gmail.com>
2024-11-07 11:02:34 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['benscott.dev'], // Add the external domain here
+  },
 };
 
+
 export default nextConfig;
+
